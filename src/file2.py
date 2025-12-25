d633d5143018274d2394c800b4794b56a6735cda
sagarsinghchauhan <singhsagarc@gmail.com>
2025-12-25 16:53:43 +0530

    update till mlflow autolog

---
+++ src/file2.py
@@ -0,0 +1,69 @@
+import mlflow
+import mlflow.sklearn
+from sklearn.datasets import load_wine
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, confusion_matrix
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# import dagshub
+import dagshub
+dagshub.init(repo_owner='sagarsinghchauhan', repo_name='MLFLOW-Experiments-with-MLOPS', mlflow=True)
+
+mlflow.set_tracking_uri("https://dagshub.com/sagarsinghchauhan/MLFLOW-Experiments-with-MLOPS.mlflow")
+
+# Load Wine dataset
+wine = load_wine()
+X = wine.data
+y = wine.target
+
+# Train test split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.10, random_state=42)
+
+# Define the params for RF model
+max_depth = 8
+n_estimators = 10
+
+# Mention your experince below
+mlflow.set_experiment('exp2')
+# with mlflow.start_run(experiment_id = 1):
+with mlflow.start_run():
+    rf = RandomForestClassifier(max_depth=max_depth,
+                                n_estimators=n_estimators)
+    
+    rf.fit(X_train,y_train)
+    y_pred = rf.predict(X_test)
+    accuracy = accuracy_score(y_test,y_pred)
+
+    mlflow.log_metric("accuracy",accuracy)
+    mlflow.log_param("max_dept",max_depth)
+    mlflow.log_param("n_estimators",n_estimators)
+
+
+    print(accuracy)
+
+    # creting a cunfusion matrix plot
+    cm  = confusion_matrix(y_test,y_pred)
+    plt.figure(figsize = (6,6))
+    sns.heatmap(cm,annot = True, fmt = 'd', cmap = 'Blues',xticklabels=wine.target_names,yticklabels=wine.target_names)
+    plt.ylabel("Actual")
+    plt.xlabel("Predicted")
+    plt.title("Confusion Matrix")
+    # plt.show()
+
+    #save plot 
+    plt.savefig("Confusion_matirx.png")
+
+    # log artifact using mlflow
+    mlflow.log_artifact("Confusion_matirx.png")
+    mlflow.log_artifact(__file__)
+
+    #tags
+    mlflow.set_tags({"author": "Sagar","project": "Wine Classification"})
+
+    #log the model 
+    mlflow.sklearn.log_model(rf,"Random-Forest-Model")
+
+
+
